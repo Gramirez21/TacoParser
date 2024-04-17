388a34abf8e55920d0769a25498d72a3acf95729
Gramirez21 <r.dannysixty@gmail.com>
2024-04-17 09:48:54 -0700

    tacoParer

--- LoggingKata.Test/TacoParserTests.cs
+++ LoggingKata.Test/TacoParserTests.cs
@@ -8,53 +8,47 @@ namespace LoggingKata.Test
         [Fact]
         public void ShouldReturnNonNullObject()
         {
-            //Arrange
+            
             var tacoParser = new TacoParser();
 
-            //Act
+            
             var actual = tacoParser.Parse("34.073638, -84.677017, Taco Bell Acwort...");
 
-            //Assert
             Assert.NotNull(actual);
 
         }
 
         [Theory]
         [InlineData("34.073638, -84.677017, Taco Bell Acwort...", -84.677017)]
-        //Add additional inline data. Refer to your CSV file.
+        
         public void ShouldParseLongitude(string line, double expected)
         {
-            // TODO: Complete the test with Arrange, Act, Assert steps below.
-            //       Note: "line" string represents input data we will Parse 
-            //       to extract the Longitude.  
-            //       Each "line" from your .csv file
-            //       represents a TacoBell location
+           
 
-            //Arrange
+            
 
             var tacoParser = new TacoParser();
 
-            //Act
+            
 
             var actual = tacoParser.Parse(line);
 
-            //Assert
+            
 
             Assert.Equal(expected, actual.Location.Longitude);
         }
 
 
-        //TODO: Create a test called ShouldParseLatitude
-
+        
         [Theory]
-        [InlineData("34.073638, -84.677017, Taco Bell Acwort...", -84.677017)]
+        [InlineData("34.073638, -84.677017, Taco Bell Acwort...", 34.073638)]
         public void ShouldParseLatitude(string line, double expected)
         {
             var tacoParser1 = new TacoParser();
 
-            var actual = tacoParser1.Parse(line);      
+            var actual = tacoParser1.Parse(line);
 
-            Assert.Equal(expected, actual.Location.Longitude);
+            Assert.Equal(expected, actual.Location.Latitude);
         }
 
     }
--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -13,46 +13,33 @@
         {
             logger.LogInfo("Begin parsing");
 
-            // Take your line and use line.Split(',') to split it up into an array of strings, separated by the char ','
+            
             var cells = line.Split(',');
 
-            // If your array's Length is less than 3, something went wrong
             if (cells.Length < 3)
             {
-                // Log error message and return null
+                
                 return null; 
             }
 
-            // TODO: Grab the latitude from your array at index 0
-            // You're going to need to parse your string as a `double`
-            // which is similar to parsing a string as an `int`
+           
 
             var latitude = double.Parse(cells[0]);
             
             
-            // TODO: Grab the longitude from your array at index 1
-            // You're going to need to parse your string as a `double`
-            // which is similar to parsing a string as an `int`
-            
+           
             var longitude = double.Parse(cells[1]);
 
-            // TODO: Grab the name from your array at index 2
-
+            
             var name = cells[2];
             
 
-            // TODO: Create a TacoBell class // done
-            // that conforms to ITrackable // done
-            
-            //done  Create an instance of the Point Struct
-            // done TODO: Set the values of the point correctly (Latitude and Longitude) 
-
+          
             var point = new Point();
 
             point.Longitude = longitude;
             point.Latitude = latitude;
-            // done TODO: Create an instance of the TacoBell class
-            // done TODO: Set the values of the class correctly (Name and Location)
+           
 
                var tacoBell = new TacoBell();
 
@@ -60,8 +47,7 @@
 
             tacoBell.Location = point;
 
-            // done TODO: Then, return the instance of your TacoBell class,
-            //    since it conforms to ITrackable
+           
 
             return tacoBell;
         }
